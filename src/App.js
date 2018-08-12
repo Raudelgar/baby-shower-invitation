0e65c9c2db5ff65649243b502e60ca1d14ba6b82
Raudel Garcia <raudel24@gmail.com>
2018-08-12 18:20:24 -0400

    Adding background image and some sections as a template

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to Daniel Baby Shower</h1>
-        </header>
-        <p className="App-intro">
-        Some text and body when Marjorie decide which design she wants :)
-        </p>
+        <section className="sectionOne">
+          <p>This an example text on Section One for the Google Fonts</p>
+        </section>
+        <section className="sectionTwo">
+          <p>This an example text on Section Two for the Google Fonts</p>
+        </section>
+        <section className="sectionThree">
+
+        </section>
       </div>
     );
   }
